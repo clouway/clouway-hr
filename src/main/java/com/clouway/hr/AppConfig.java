e2eb56841af19b0402796e914629fce32a4d0a2e
Panayot <panayotkulchev@gmail.com>
2015-07-15 11:18:01 +0300

    skeleton

--- src/main/java/com/clouway/hr/AppConfig.java
+++ src/main/java/com/clouway/hr/AppConfig.java
@@ -1,16 +1,9 @@
 package com.clouway.hr;
 
-import com.clouway.hr.adapter.db.PersistentVacationRepository;
-import com.clouway.hr.core.VacationRepository;
-import com.google.inject.AbstractModule;
+import com.clouway.hr.adapter.persistence.PersistenceModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.Provides;
 import com.google.inject.servlet.GuiceServletContextListener;
-import com.google.inject.servlet.ServletModule;
-import com.google.sitebricks.SitebricksModule;
-import com.vercer.engine.persist.ObjectDatastore;
-import com.vercer.engine.persist.annotation.AnnotationObjectDatastore;
 
 /**
  * @author Dimitar Dimitrov (dimitar.dimitrov045@gmail.com)
@@ -18,35 +11,6 @@ import com.vercer.engine.persist.annotation.AnnotationObjectDatastore;
 public class AppConfig extends GuiceServletContextListener {
   @Override
   protected Injector getInjector() {
-    return Guice.createInjector(
-
-            new SitebricksModule() {
-              @Override
-              protected void configureSitebricks() {
-                scan(AppConfig.class.getPackage());
-
-              }
-            },
-
-            new ServletModule() {
-              @Override
-              protected void configureServlets() {
-
-
-              }
-            },
-
-            new AbstractModule() {
-              @Override
-              protected void configure() {
-                bind(VacationRepository.class).to(PersistentVacationRepository.class);
-
-              }
-
-              @Provides
-              public ObjectDatastore getDataStore() {
-                return new AnnotationObjectDatastore();
-              }
-            });
+    return Guice.createInjector(new PersistenceModule());
   }
 }
