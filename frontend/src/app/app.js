f18d46e395c13ac817071581b375b7e33f7d32b5
clouway <dimitar.dimitrov045@gmail.com>
2015-07-16 11:59:28 +0300

    http wrapper with tests

--- frontend/src/app/app.js
+++ frontend/src/app/app.js
@@ -1,21 +1,50 @@
-angular.module( 'hr.core', [
+angular.module('hr.core', [
   'templates-app',
   'templates-common',
   'ui.router'
 ])
+        .config(function myAppConfig($stateProvider, $urlRouterProvider) {
+          $urlRouterProvider.otherwise('/home');
+        })
 
-.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
-  $urlRouterProvider.otherwise( '/home' );
-})
+        .run(function run() {
+        })
 
-.run( function run () {
-})
+        .controller('AppCtrl', function AppCtrl($scope) {
+          $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+            if (angular.isDefined(toState.data.pageTitle)) {
+              $scope.pageTitle = toState.data.pageTitle;
+            }
+          });
+        })
 
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
-  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-    if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle;
-    }
-  });
-})
-;
+        .service('httpRequest', ['$http', '$q', '$rootScope', function ($http, $q, $rootScope) {
+
+          this.get = function (url, data) {
+            return this.send('GET', url, data);
+          };
+
+          this.post = function (url, data) {
+            return this.send('POST', url, data);
+          };
+
+          this.put = function (url, data) {
+            return this.send('PUT', url, data);
+          };
+
+          this.send = function (method, url, data) {
+            var deferred = $q.defer();
+
+            $http({method: method, url: url, data: data})
+                    .success(function (data) {
+                      deferred.resolve(data);
+                    })
+                    .error(function (data) {
+                      deferred.reject(data);
+                    });
+
+            $rootScope.loadingInProgress = true;
+
+            return deferred.promise;
+          };
+        }]);
